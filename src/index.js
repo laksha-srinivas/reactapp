41790ff46f43fd77c18a1894777ca05148f4a592
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-03-03 23:23:24 +0100

    model view intents

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import ReactDom from 'react-dom';
 import './index.css';
 //import React{ Component } from 'react'
 import { Route, BrowserRouter, Link } from 'react-router-dom'
+
+let model ={
+    running:false,
+    time: 0
+};
+
+let view =(model) => <div> {model.time} </div>;
+
+let intents= {
+    TICK : 'TICK',
+    START : 'START',
+    STOP : 'STOP',
+    RESET : 'RESET',
+};
+
+ReactDom.render (view(model)),
+    document.getElementById('root'
+    );
+
+let view = (model) => {
+    let minutes = Math.floor(model.time / 60);
+    
+}
