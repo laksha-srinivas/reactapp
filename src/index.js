77cd6cf67b90afbe24e7562f2ee42c81de040523
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-03-04 13:25:36 +0100

    model intent program attempt1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-//import React{ Component } from 'react'
 import { Route, BrowserRouter, Link } from 'react-router-dom'
 
 let model ={
@@ -9,7 +8,21 @@ let model ={
     time: 0
 };
 
-let view =(model) => <div> {model.time} </div>;
+//let view =(model) => <div> {model.time} </div>;
+
+let view = (m) => {
+    let minutes = Math.floor(m.time / 60);
+    let seconds = m.time - (minutes * 60);
+    let secondsFormatted = `$ {seconds < 10 ? '0' : ' '} $ {seconds}`;
+    let handler = (event) => {
+        model = update (model, m.running ? 'STOP' : 'START');
+    };
+
+    return <div>
+    <p> {minutes} : {secondsFormatted} </p>
+     <button onClick = {handler}> {m.running ? 'Stop' : 'Start'} </button>
+     </div>;
+};
 
 let intents= {
     TICK : 'TICK',
@@ -18,11 +31,24 @@ let intents= {
     RESET : 'RESET',
 };
 
-ReactDom.render (view(model)),
-    document.getElementById('root'
-    );
+const update = (model, intent) => {
+    const updates = {
+        'START' : (model) => Object.assign (model, {running: true}),
+        'STOP' : (model) => Object.assign (model, {running : false}),
+        //'TICK' : (model) => Object.assign (model, {time: model.time + 1})
+        'TICK' : (model) => Object.assign (model, {time: model.time + (model.running ? 1 : 0)})
+    }; //add 1 if true else add 0
+    return updates[intent] (model);
+};
 
-let view = (model) => {
-    let minutes = Math.floor(model.time / 60);
-    
-}
+const render = () => {
+    ReactDom.render (view (model),
+        document.getElementById('root')
+    );
+   };
+   render();
+
+setInterval(() => {
+    model = update(model ,'TICK');
+    render();
+    },1000);
