b03e452a47772508df3b310c49aeed5c14d00494
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-03-06 17:05:49 +0100

    last commit of react fundamentals

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 import { Route, BrowserRouter, Link } from 'react-router-dom'
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
