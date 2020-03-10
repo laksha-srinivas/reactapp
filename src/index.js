5815a11856d556b4a6fa6d65e714dc1e644489fd
Laksha Srinivas <lakshasrinivas3546@gmail.com>
2020-03-10 22:13:59 +0100

    React - First practise program

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import './index.css';
 import { Route, BrowserRouter, Link } from 'react-router-dom'
 import * as Redux from 'redux';
 import * as ReactRedux from 'react-redux';
+
+function Hello()
+{
+    return<div> Hello React! </div>
+}
+
+ReactDom.render(
+    <Hello/>,
+    document.getElementById('root')
+);
